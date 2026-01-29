bff7ce1c500174035c3859c8efce6fc6191317ff
Christian Dybro <cdybro@gmail.com>
2026-01-29 17:38:17 +0100

    1st release of html release manager

---
+++ data.js
@@ -0,0 +1,12 @@
+// data.js - Resource Management Data
+const rawData = [
+    ["Resource 1", "Vendor 1", "Lead Developer", "Team Alpha", "Section 1", 1.0, "Active"],
+    ["Resource 2", "Vendor 1", "Scrum Master", "Team Alpha", "Section 1", 0.5, "Active"],
+    ["Resource 3", "Internal", "Product Owner", "Team Alpha", "Section 1", 1.0, "Active"],
+    ["Resource 4", "Vendor 2", "UX Designer", "Team Beta", "Section 2", 0.8, "Onboarding"],
+    ["Resource 5", "Vendor 2", "Developer", "Team Beta", "Section 2", 1.0, "High Load"],
+    ["Resource 6", "Internal", "System Architect", "Team Gamma", "Section 3", 1.0, "Active"],
+    ["Resource 7", "Vendor 3", "Data Analyst", "Team Gamma", "Section 3", 1.0, "Leave"],
+    ["Resource 8", "Vendor 1", "Backend Dev", "Team Delta", "Section 4", 1.2, "High Load"],
+    ["Resource 9", "Internal", "Team Lead", "Team Delta", "Section 4", 1.0, "Inactive"]
+];
